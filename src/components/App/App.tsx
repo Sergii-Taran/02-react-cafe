202b23f7ab2af4b06bd110a59f0edda4d701f9a3
Sergii Taran <serg.taran1970@gmail.com>
2026-02-26 09:47:29 +0200

    feat: add VoteStats component with dynamic statistics

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@ import css from './App.module.css';
 import CafeInfo from '../CafeInfo/CafeInfo';
 import type { Votes, VoteType } from '../../types/votes';
 import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
@@ -26,10 +27,20 @@ export default function App() {
     });
   };
 
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+  const positiveRate =
+    totalVotes === 0 ? 0 : Math.round((votes.good / totalVotes) * 100);
+
   return (
     <div className={css.app}>
       <CafeInfo />
       <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+      <VoteStats
+        votes={votes}
+        totalVotes={totalVotes}
+        positiveRate={positiveRate}
+      />
     </div>
   );
 }
